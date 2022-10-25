52dd8a031740be41421d26e53e82654982a34ce3
jean <jean@glowforthgames.com>
2022-10-25 21:35:37 +0900

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
